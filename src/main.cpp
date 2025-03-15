5f642cea1fcb5e3dec501551a2366fd5e280c4ec
xiyang <1042335226@qq.com>
2025-03-15 15:45:06 +0800

    spdlikeprintf

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/bundled/printf.h>
+
+template <class loggerPtr, class... Args>
+void loglineprintf(loggerPtr                 logger,
+    spdlog::level::level_enum level,
+    spdlog::source_loc        loc,
+    const char* fmt,
+    const Args&... args) noexcept
+{
+    if (logger && logger->should_log(level))
+    {
+        logger->log(loc, level, "{}", fmt::sprintf(fmt, args...));
+    }
+}
+
+#define SPDLOG_LOGGER_PRINTF(logger, level, ...) \
+    loglineprintf(logger, level, spdlog::source_loc{__FILE__, __LINE__, SPDLOG_FUNCTION}, __VA_ARGS__)
+
+//specific log implementation macros
+
+#define LOG_ERROR(...) SPDLOG_LOGGER_PRINTF(spdlog::default_logger(),spdlog::level::err,__VA_ARGS__)
+#define LOG_INFO(...) SPDLOG_LOGGER_PRINTF(spdlog::default_logger(),spdlog::level::info,__VA_ARGS__)
+
+int main()
+{
+    int a = 10;
+    LOG_ERROR("hello {}", a);  // 使用 fmt 风格的格式化
+    LOG_ERROR("hello %d", a);  // 使用 printf 风格的格式化，修正为 %d
+    return 0;
+}
